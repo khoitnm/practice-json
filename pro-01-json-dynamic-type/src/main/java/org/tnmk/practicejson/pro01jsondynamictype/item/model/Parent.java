84832174885e346dcbeb57dacb413c84a5f26c7e
trank <Kevin.Tran@pointclickcare.com>
2022-03-02 08:22:33 -0500

    Remove some redundant code; add lombok dependencies.

---
+++ pro-01-json-dynamic-type/src/main/java/org/tnmk/practicejson/pro01jsondynamictype/item/model/Parent.java
@@ -0,0 +1,5 @@
+package org.tnmk.practicejson.pro01jsondynamictype.item.model;
+
+public class Parent {
+  private Object child;
+}
--- pro-01-json-dynamic-type/src/test/java/org/tnmk/practicejson/pro01jsondynamictype/common/test/TestType.java
+++
@@ -1,7 +0,0 @@
-package org.tnmk.practicejson.pro01jsondynamictype.common.test;
-
-public class TestType {
-  public static final String UNIT_TEST = "UnitTest";
-  public static final String COMPONENT_TEST = "ComponentTest";
-  public static final String CONTRACT_TEST = "ContractTest";
-}
